40d17a45c0ba4b447bda747270ab5b7eed3526cf
Suhaa Mahmood <suhaa786@outlook.com>
2025-07-19 15:01:31 +0100

    Add delete operation for beneficiary

--- backend/src/routes/beneficiaryRoutes.ts
+++ backend/src/routes/beneficiaryRoutes.ts
@@ -1,6 +1,6 @@
 // --- src/routes/beneficiary.routes.ts ---
 import express from 'express';
-import { createBeneficiary, getBeneficiaries, updateBeneficiary } from '../controllers/beneficiaryController';
+import { createBeneficiary, getBeneficiaries, updateBeneficiary, deleteBeneficiary } from '../controllers/beneficiaryController';
 import { verifyToken } from '../middlewares/verifyToken';
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.use(verifyToken);
 router.post('/', createBeneficiary);
 router.get('/', getBeneficiaries);
 router.put('/:id', updateBeneficiary);
+router.delete('/:id', deleteBeneficiary);
 
 export default router;
