af81ec2f8b17ae8d96cd4c57f44948091b2661a8
shiinedev <shiinecadde17@gmail.com>
2025-07-20 21:02:12 +0100

    added App in main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -22,6 +22,7 @@ createRoot(document.getElementById('root')!).render(
           <BeneficiariesProvider>
             <TrainingProvider>
               <RouterProvider router={router} />
+              <App />
             </TrainingProvider>
           </BeneficiariesProvider>
         </AuthProvider>
